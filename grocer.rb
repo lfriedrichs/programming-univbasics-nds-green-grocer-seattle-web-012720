220692f79c26887d432d5cc568dd2b96e62f0482
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-16 02:02:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ def apply_clearance(cart)
   index = 0 
   while index < cart.size 
     if cart[index][:clearance] == true
-      (cart[index][:price] *= 0.8).round(2)
+      cart[index][:price] = cart[index][:price]*0.8
     end
     index += 1 
   end
